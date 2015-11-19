2496fbeddc5d9f070da9ec692eae1b54d3d6f856
ottogin <lukartoil@mail.ru>
2015-11-20 00:44:26 +0300

    MY_CPU_assembler

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "stack.c"
+#include "assembler.c"
+
+
+int main()
+{
+	printf("\n\n//////////////////////////////\n\n");
+	translate(NULL, NULL);
+	printf("\n\n//////////////////////////////\n");
+	decrypt(NULL, NULL);
+	return 0;
+}
